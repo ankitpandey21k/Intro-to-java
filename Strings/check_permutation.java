761f3b6fc86d6fb1c2af346cc5fee94e5fcd71f9
ankitpandey21k <89537954+ankitpandey21k@users.noreply.github.com>
2023-03-21 23:52:08 +0530

    Add files via upload

---
+++ Strings/check_permutation.java
@@ -0,0 +1,38 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class check_permutation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		String str=scn.nextLine();
+		String str1=scn.nextLine();
+		
+		boolean ans=ispermutation(str,str1);
+		System.out.println(ans);
+
+	}
+
+	public static boolean ispermutation(String str, String str1) {
+		// TODO Auto-generated method stub
+		int arr1[] = new int[26];
+		for (int i = 0; i < str.length(); i++) {
+			int a = ((int) str.charAt(i) - 97);
+			arr1[a] += 1;
+		}
+		int arr2[] = new int[26];
+		for (int i = 0; i < str1.length(); i++) {
+			int a = ((int) str1.charAt(i) - 97);
+			arr2[a] += 1;
+		}
+		for(int i = 0 ;i<26; i++){
+			if(arr1[i]!=arr2[i]){
+				return false;
+			}
+		}
+		 
+		return true;
+	}
+}
