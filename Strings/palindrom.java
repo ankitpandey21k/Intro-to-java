ec62b884cdce98355e90a852d392a419aec76166
ankitpandey21k <89537954+ankitpandey21k@users.noreply.github.com>
2023-03-21 18:01:01 +0530

    Update palindrom.java

--- Strings/palindrom.java
+++ Strings/palindrom.java
@@ -11,9 +11,6 @@ public class palindrom {
 		else {
 			System.out.println(str+" is not palindrome string");
 		}
-		
-	
-
 	}
 
 	public static void main(String[] args) {
@@ -23,4 +20,40 @@ public class palindrom {
 		
 	}
 
+}
+************************************************************************************************************************************************************************
+---------------------------------------------String Palindrome-----------------------------------------String Palindrome------------------------------------------------
+**********************************************************************************************************************************************************************	
+Given a string, determine if it is a palindrome, considering only alphanumeric characters.
+Palindrome
+A palindrome is a word, number, phrase, or other sequences of characters which read the same backwards and forwards.
+	Input Format:
+The first and only line of input contains a string without any leading and trailing spaces. All the characters in the string would be in lower case.
+       Output Format:
+The only line of output prints either 'true' or 'false'.
+	
+	Sample Input 1 :
+abcdcba
+      Sample Output 1 :
+true 
+     Sample Input 2:
+coding
+     Sample Output 2:
+false
+	
+	public class Solution {
+
+	public static boolean isPalindrome(String str) {
+		// Your code goes here
+		int i = 0;
+		int n = str.length() - 1;
+		while (i <= n) {
+			if (str.charAt(i) != str.charAt(n)) {
+				return false;
+			}
+			i++;
+			n--;
+		}
+		return true;
+	}
 }
