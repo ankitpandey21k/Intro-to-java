52a133a14c329e4769a0bf8598a1949cc68e14f2
ankitpandey21k <89537954+ankitpandey21k@users.noreply.github.com>
2023-03-22 00:20:53 +0530

    Update Countwords.java

--- Strings/Countwords.java
+++ Strings/Countwords.java
@@ -19,3 +19,13 @@ public class Countwords {
 	      System.out.print("Number of words are present:"+count);
 	}
 }
+*********************************************************************************************************************************************************************
+public static int countWords(String str) {	
+		//Your code goes here
+		if (str.length() == 0 ){
+			return 0; 
+		}
+		String arr[] = str.split(" ");   //string split() function is a method that allows you to split a string into an array of substrings based on a specified delimiter.
+		return arr.length;
+	}
+	
