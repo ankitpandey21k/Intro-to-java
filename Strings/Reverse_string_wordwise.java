20188b7c49e0d9f0ce69750723f6d70ce4fb8849
ankitpandey21k <89537954+ankitpandey21k@users.noreply.github.com>
2023-03-22 02:00:52 +0530

    Update Reverse_string_wordwise.java

--- Strings/Reverse_string_wordwise.java
+++ Strings/Reverse_string_wordwise.java
@@ -40,5 +40,17 @@ public class Reverse_string_wordwise {
 
 		}
 	}
-
+***************************************************************************************************************************************************************
+        int end=input.length();
+	int i=input.length()-1;
+	String output="";
+	while(i>=0){
+		if( input.charAt(i)==' '){
+			output+=input.substring(i+1,end)+" ";
+			end=i;
+		}
+		i--;
+	}
+	output+=input.substring(i+1,end);
+	return output;
 
