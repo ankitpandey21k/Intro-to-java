78a1278654efafd42419fce04da83acec33aac1a
ankitpandey21k <89537954+ankitpandey21k@users.noreply.github.com>
2023-03-22 11:33:56 +0530

    Add files via upload

---
+++ Strings/Remove_character.java
@@ -0,0 +1,28 @@
+package Strings;
+
+import java.util.Scanner;
+
+public class Remove_character {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner scn=new Scanner(System.in);
+        String str=scn.nextLine();
+        char ch=scn.next().charAt(0);
+        String ans=removeCharacter(str,ch);
+        System.out.println(ans);
+	}
+
+	private static String removeCharacter(String str, char ch) {
+		// TODO Auto-generated method stub
+		String ans="";
+		for(int i=0;i<str.length();i++) {
+			
+			if(str.charAt(i)!=ch) {
+				ans+=str.charAt(i);
+			}
+		}
+		return ans;
+	}
+
+}
